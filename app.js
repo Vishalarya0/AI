7de796d3177ed9ab43186815a2510741c78316fb
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-07 06:33:41 +0530

    add dotenv file

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 import Groq from "groq-sdk";
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+import dotenv from "dotenv";
+
+dotenv.config();
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY?.trim()});
+
+
+async function main() {
+    try {
+    const completion= await groq.chat.completions.create({
+        model: "llama-3.3-70b-versatile",
+        messages: [
+            { role: "user", content: "Hello, world!" }
+        ]
+    });
+
+    console.log("completion", completion);} 
+
+
+
+
+    
+    catch (error) {
+    console.error("Error creating chat completion:", error.messages);
+}
+}
+
+main();
