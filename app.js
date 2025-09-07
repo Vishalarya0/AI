8f1cbb2273d72bb2e27448bdb8b5fe603bfc8e03
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-07 07:46:27 +0530

    add  temperature: 0.1,

--- app.js
+++ app.js
@@ -8,18 +8,23 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY?.trim()});
 async function main() {
     try {
     const completion= await groq.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
+     
+        temperature: 0.1,
+        max_tokens: 1000,
+
+        model: 'llama-3.3-70b-versatile',
         messages: [
-            { role: "user", content: "Hello, world!" }
+            { role: 'system', content: " hii" },
+            { role: 'user', content: "hii " }
         ]
     });
 
-    console.log("completion", completion);} 
+    console.log("completion", completion.choices[0].message.content);} 
 
 
 
 
-    
+
     catch (error) {
     console.error("Error creating chat completion:", error.messages);
 }
